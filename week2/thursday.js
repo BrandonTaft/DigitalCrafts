504c6d4d383fa0bdd716ba55b31c473ae96fefd2
Btaft6278 <btaft6278@gmail.com>
2021-09-21 19:12:51 -0400

    added week2 to DigitalCraft repo

---
+++ week2/thursday.js
@@ -0,0 +1,33 @@
+
+const clickMe =document.getElementById("clickMe")
+const newsURL ='https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+let storyID = 28078575
+const idURL = `https://hacker-news.firebaseio.com/v0/item/${storyID}.json?print=pretty`
+const ulID = document.getElementById
+
+async function getAllNews(){
+   let response = await fetch(newsURL)
+   let allNews = await response.json()
+   console.log(allNews)
+   }
+
+async function getStory(storyID){
+    let response = await fetch(idURL)
+    let theStory = await response.json()
+    console.log(theStory)
+  
+}
+ function displayNews(){
+     let newsDisplay = `
+     <ul>${theStory} </ul>`
+   
+   
+    ulID.innerHTML = newsDisplay.join("")   
+}
+
+
+clickMe.addEventListener("click", function(){
+getAllNews()
+getStory(storyID)
+displayNews()
+})
